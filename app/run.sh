e2c618776257c4781d4e20c529b089253e49464c
Matt Fay <matteosox@gmail.com>
2021-03-30 16:04:26 -0700

    - Completed the NextJS tutorial to setup a simple app. - Edited the `app/run.sh` bash script to be able to run arbitrary commands in the app container. - Added `.npm` to the app's `.gitignore` since it showed up. - Reformatted the `notebooks/run.sh` bash script.

--- app/run.sh
+++ app/run.sh
@@ -1,8 +1,36 @@
 #! /usr/bin/env bash
-set -euf -o pipefail
+set -ef -o pipefail
 
 TAG=$(git rev-parse --short HEAD)
-echo "Running app for tag $TAG"
 DIR="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
+CMD="npm run dev"
 
-docker run -p 3000:3000 -v $DIR:/home/app app:$TAG npm run dev
+usage()
+{
+    echo "usage: ./run.sh [--tag -t sha=$TAG] [--command -c cmd=$CMD"
+}
+
+while [ "$1" != "" ]; do
+    case "$1" in
+        -t | --tag )
+            shift
+            TAG="$1"
+            ;;
+        -c | --command )
+            shift
+            CMD="$1"
+            ;;
+        -h | --help )
+            usage
+            exit
+            ;;
+        * )
+        usage
+        exit 1
+    esac
+    shift
+done
+
+echo "Running app for tag $TAG"
+
+docker run -p 3000:3000 -v "$DIR":/home/app app:"$TAG" $CMD
