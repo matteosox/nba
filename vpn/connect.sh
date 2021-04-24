bebedf9e61971756829ac4e2a9cecced5cd8b814
Matt Fay <matteosox@gmail.com>
2021-04-23 17:13:57 -0700

    Kinda flying blind here, but simplified `connect.sh` and added more logging still.

--- vpn/connect.sh
+++ vpn/connect.sh
@@ -13,9 +13,15 @@ usage() {
 }
 
 cleanup() {
-    echo "Deleting secret files"
+    if [[ -e openvpn.log ]]; then
+        echo "Printing out openvpn logs"
+        cat openvpn.log
+    else
+        echo "No openvpn logs to print out"
+    fi
+    echo "Cleaning up"
     rm -f .credentials ca.crt ta.key
-    echo "Secret files deleted"
+    echo "Cleanup complete"
 }
 trap cleanup EXIT
 
@@ -54,13 +60,13 @@ while [[ $# -gt 0 ]]; do
             ;;
         * )
             CONFIG_PATH="$1"
-            echo "----Using config file found at $CONFIG_PATH----"
+            echo "Using config file found at $CONFIG_PATH"
             shift
             ;;
     esac
 done
 
-echo "----Setting up credentials, certificate authority, and TLS auth files----"
+echo "Setting up credentials, certificate authority, and TLS auth files"
 echo -e "$VPN_USERNAME\n$VPN_PASSWORD" >> .credentials
 chmod 600 .credentials
 echo -e "$VPN_ROOT_CERT" >> ca.crt
@@ -68,10 +74,9 @@ chmod 600 ca.crt
 echo -e "$VPN_TLS_AUTH_KEY" >> ta.key
 chmod 600 ta.key
 
-echo "----Initiating openvpn connection----"
+echo "Initiating openvpn connection"
 touch openvpn.log
 sudo openvpn --config "$CONFIG_PATH" --auth-user-pass .credentials --ca ca.crt --tls-auth ta.key 1 --log-append openvpn.log --daemon
-tail --pid "$$" -n +1 -F openvpn.log &
-timeout 30 bash -c 'tail -n +1 -F openvpn.log | grep -q -m 1 "Initialization Sequence Completed"' || (EXIT_CODE="$?" && echo "----ERROR: Timeout reached, unable to connect, eith exit code $EXIT_CODE----" && exit "$EXIT_CODE")
+timeout 30 bash -c 'tail -n +1 -F openvpn.log | grep -q -m 1 "Initialization Sequence Completed"' || (EXIT_CODE="$?" && echo "ERROR: Unable to connect, eith exit code $EXIT_CODE" && exit "$EXIT_CODE")
 
-echo "----All done!----"
+echo "Connection successfully initiated!"
