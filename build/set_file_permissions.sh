9609001d16fb01f80738fd8f8ae65f7551c30667
Matt Fay <matteosox@gmail.com>
2021-04-25 00:50:37 -0700

    Update permissions slightly, make logging slightly more legible.

--- build/set_file_permissions.sh
+++ build/set_file_permissions.sh
@@ -8,7 +8,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR"/..
 
 # Set data directory permissions to allow r/w for all users
-find data -type d -exec chmod 766 {} \;
+find data -type d -exec chmod 777 {} \;
 
 # We generate the requirements/requirements.txt in Docker, so it must be writeable
-chmod 666 requirements/requirements.txt
+chmod 766 requirements/requirements.txt
