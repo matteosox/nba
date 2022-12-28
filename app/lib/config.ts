fe000fe7aea2e4d5987c6ad479a61ab14f56a9d1
Matt Fay <matt.e.fay@gmail.com>
2022-12-27 23:07:02 -0800

    Don't use confme since it doesn't support typescript, use a simple config object

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,5 +1,8 @@
-import confme from "confme"
-import path from "path"
-
-
-export default confme(path.join(process.cwd(), "config.json"))
+export default {
+    awsS3Region: "us-west-2",
+    awsS3Bucket: "nba-mattefay",
+    awsS3RootKey: "prod",
+    awsAccessKeyId: process.env.AccessKeyId,
+    awsSecretAccessKey: process.env.SecretAccessKey,
+    useLocal: !!process.env.USE_LOCAL
+}
